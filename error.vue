24c844d4dde31b3b77bf7c8b02dea883b0517cbc
Ali  Aryaeifar <aryaeifar12@gmail.com>
2024-06-30 18:07:23 +0330

    add error 404 page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="error">
+    <div class="error-wrapper">
+      <div class="error-wraper_404 mb-4">404</div>
+      <div class="error-wraper_sub mb-4">متاسفانه صفحه مورد شما یافت نشد</div>
+      <button class="error-wraper_btn"> 
+        <a href="/">برگشت به خانه</a>  
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
