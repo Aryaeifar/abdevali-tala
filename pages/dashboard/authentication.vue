b0471a4d4f081deea125677b770fa31fb6ee08a7
Ali  Aryaeifar <aryaeifar12@gmail.com>
2024-07-07 22:23:49 +0330

    add dashboard-auth page

--- pages/dashboard/authentication.vue
+++ pages/dashboard/authentication.vue
@@ -5,46 +5,117 @@
       <p class="dashboard-auth_wrapper-text mb-6">
         به بخش احراز هویت خوش آمدید!
       </p>
-      <p class="dashboard-auth_wrapper-text mb-6">
+      <p class="dashboard-auth_wrapper-text mb-10">
          لطفاً اطلاعات زیر را برای تأیید هویت خود وارد کنید.
       </p>
-      <div class="dashboard-auth_wrapper-input mb-6">
-        <label for="idCard">شماره کارت ملی</label>
-        <input type="text" id="idCard" name="idCard" />
-      </div>
-      <div class="dashboard-auth_wrapper-input mb-6">
-        <label for="bankCard">شماره کارت بانکی</label>
-        <input type="text" id="bankCard" name="bankCard" />
-      </div>
-      <div class="dashboard-auth_wrapper-input-expire mb-6">
-        <label for="">تاریخ انقضاء کارت بانکی (ماه/سال)</label>
-        <div class="dashboard-auth_wrapper-expire-inputs">
-          <input type="text" class="dashboard-auth_wrapper-expire-input" />
-          <span class="icon-line"></span>
-          <input type="text" class="dashboard-auth_wrapper-expire-input" />
-        </div>
-      </div>
-      <div class="dashboard-auth_wrapper-upload d-flex justify-space-between mb-6">
-        <p class="dashboard-auth_wrapper-text mb-6">
+      <v-row class="dashboard-auth_inputs">
+        <v-col cols="4" sm="3" lg="2">
+          <label for="idCard" class="dashboard-auth-label"
+            >شماره کارت ملی</label
+          >
+        </v-col>
+        <v-col cols="8" sm="9" lg="4">
+          <input
+            type="text"
+            id="idCard"
+            name="idCard"
+            class="dashboard-auth-input"
+          />
+        </v-col>
+      </v-row>
+      <v-row class="dashboard-auth_inputs">
+        <v-col cols="4" sm="3" lg="2">
+          <label for="idbankCardCard" class="dashboard-auth-label"
+            >شماره کارت بانکی</label
+          >
+        </v-col>
+        <v-col cols="8" sm="9" lg="4">
+          <input
+            type="text"
+            id="bankCard"
+            name="bankCard"
+            class="dashboard-auth-input"
+          />
+        </v-col>
+      </v-row>
+      <v-row class="dashboard-auth_inputs mb-6">
+        <v-col cols="8" sm="5" lg="3">
+          <label for="idCard" class="dashboard-auth-label"
+            >تاریخ انقضاء کارت بانکی (ماه/سال)</label
+          >
+        </v-col>
+        <v-col cols="12" sm="7" lg="3">
+          <div class="dashboard-auth-input-expire">
+            <input
+              type="text"
+              id="month"
+              name="month"
+              class="input-field"
+              maxlength="2"
+              v-model="input1"
+              @input="handleInput(1)"
+            />
+            <span class="icon-line"></span>
+            <input
+              type="text"
+              id="year"
+              name="year"
+              class="input-field"
+              maxlength="2"
+              v-model="input2"
+              @input="handleInput(2)"
+            />
+          </div>
+        </v-col>
+      </v-row>
+      <div
+        class="dashboard-auth_wrapper-upload d-flex flex-column flex-lg-row justify-space-between mb-6"
+      >
+        <p class="dashboard-auth_wrapper-text dashboard-auth_wrapper-up-text mb-6">
           پس از وارد کردن اطلاعات، لطفاً عکس‌های واضحی از کارت ملی و کارت بانکی
           خود را آپلود کنید.
         </p>
-        <v-btn flat class="dashboard-auth_wrapper-upload-btn px-6" color="Blue-100" height="3rem">
-            آپلود عکس <span class="icon-file ms-2"></span>
+        <v-btn
+          flat
+          class="dashboard-auth_wrapper-upload-btn px-6"
+          color="Blue-100"
+          height="3rem"
+          width="9rem"
+        >
+          آپلود عکس <span class="icon-file ms-2"></span>
         </v-btn>
       </div>
-      <div class=" mb-6">
-        <p class="dashboard-auth_wrapper-text">
-         <span class="text-red" >توجه</span>: اطلاعات شما با استانداردهای امنیتی بالا محافظت می‌شود و تنها برای احراز هویت شما استفاده خواهد شد.
+      <div class="mb-6">
+        <p class="dashboard-auth_wrapper-text dashboard-auth_wrapper-up-text" >
+          <span class="text-red">توجه</span>: اطلاعات شما با استانداردهای امنیتی
+          بالا محافظت می‌شود و تنها برای احراز هویت شما استفاده خواهد شد.
         </p>
       </div>
-      <v-btn flat color="Blue-100" height="3rem" class="px-6 dashboard-send-button">
+      <v-btn
+        flat
+        color="Blue-100"
+        height="3rem"
+        class="px-6 dashboard-send-button"
+      >
         ارسال اطلاعات
       </v-btn>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const input1 = ref('');
+const input2 = ref('');
+const handleInput = (inputNumber) => {
+  if (inputNumber === 1 && input1.value.length >= 2) {
+    document.querySelectorAll('.input-field')[1].focus();
+  } else if (inputNumber === 2 && input2.value.length >= 2) {
+    console.log('You reached the last input. Good job!');
+  }
+};
+onMounted(() => {
+  document.querySelector('.input-field').focus();
+});
+</script>
 
 <style lang="scss" scoped></style>
--- pages/dashboard/send-ticket.vue
+++ pages/dashboard/send-ticket.vue
@@ -14,10 +14,11 @@
           <textarea type="text" class="bg-light" rows="10"></textarea>
         </div>
       </div>
-      <div class="d-flex justify-end mt-6">
-        <v-btn class="dashboard-send_button" color="primary" flat
-          >ارسال</v-btn
-        >
+      <div class="d-flex justify-space-between mt-6">
+        <v-btn flat class="dashboard-send_upload-btn" color="Blue-100" height="3rem">
+          آپلود عکس <span class="icon-file ms-2"></span>
+        </v-btn>
+        <v-btn class="dashboard-send_button" color="primary" height="3rem"flat>ارسال</v-btn>
       </div>
     </div>
   </div>
